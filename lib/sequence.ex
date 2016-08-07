a906f0012f7a788a33c14352b2b4e2a9f9e3b725
LATBauerdick <github@latbauerdick.de>
2016-08-07 14:29:26 -0500

    sequence server and supervisors up to release management

---
+++ lib/sequence.ex
@@ -0,0 +1,9 @@
+defmodule Sequence do
+  use Application
+
+  def start(_type, _args) do
+    Sequence.Supervisor.start_link(
+      Application.get_env(:sequence, :initial_number)
+    )
+  end
+end
