b06c613cbcee5a075e3fa584b48745eb373043cf
LATBauerdick <github@latbauerdick.de>
2016-08-08 22:57:46 -0500

    fixes

--- lib/sequence.ex
+++ lib/sequence.ex
@@ -3,7 +3,9 @@ defmodule Sequence do
 
   def start(_type, _args) do
     Sequence.Supervisor.start_link(
-      { Application.get_env(:sequence, :initial_number), 1}
+      { Application.get_env(:sequence, :initial_number), 
+        Application.get_env(:sequence, :initial_delta)
+      }
     )
   end
 end
